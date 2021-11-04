2a8f7bbb021e499e23c57b9d965cabbb1c8917b3
ABD-Z <aboud2008@gmail.com>
2021-11-04 23:42:08 +0100

    -MAIN IS NOW A JUKEBOX SHOWCASE

--- examples/SFML/main.cpp
+++ examples/SFML/main.cpp
@@ -1,50 +1,93 @@
 #include "custom_sfml_stream.hpp"
-#include <vector>
+#include <cstdlib>
 #include <thread>
 #include <chrono>
 
 #include "../songs/tutorial.hpp"//include your song
-#define FILENAME "tutotrack.wav"
+#include "../songs/examples.hpp"
+
+#define REALTIME
+#define TIME_RECORDING_SECONDS 5
+#define NUMBER_OF_TRACKS 7
+
+void clear_console(){
+#ifdef __linux__
+    system("clear");
+#elif _WIN32
+    system("cls");
+#else
+
+#endif
+
+}
 
 int main() {
-    auto t1 = std::chrono::system_clock::now();
+    auto time1 = std::chrono::system_clock::now();
+    int index = 0;
+    C0deTracker::Track* t0 = new TutoTrack();
+    C0deTracker::Track* t1 = new SuperStreetFighterII_CreditTheme();
+    C0deTracker::Track* t2 = new FrereJacques();
+    C0deTracker::Track* t3 = new FZERO_MenuTheme();
+    C0deTracker::Track* t4 = new SuperMarioBros_OverworldTheme();
+    C0deTracker::Track* t5 = new KirbysDreamland_GreenGreensTheme();
+    C0deTracker::Track* t6 = new Sonic_GreenHillZoneTheme();
 
-    C0deTracker::Track* track = new TutoTrack();
-    track->init();
-    /*chans[0].disable();
-   chans[1].disable();
-    chans[2].disable();
-   chans[3].disable();
-   chans[4].disable();
-   chans[5].disable();
-  chans[6].disable();
-   chans[7].disable();*/
+    C0deTracker::Track* tracks[] = {t0,t1,t2,t3,t4,t5,t6};
+    for(auto* tr : tracks){
+        tr->init();
+    }
+    C0deTracker::Track* track;
+    track = tracks[index];
 
+    auto time2 = std::chrono::system_clock::now();
 
-    auto t2 = std::chrono::system_clock::now();
+    double deltaT = std::chrono::duration_cast<std::chrono::milliseconds>(time2 - time1).count();
 
-    double deltaT = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
-
-    std::cout << "Time for track creation = " << deltaT << " ms" << std::endl;
+    std::cout << "Time for  tracks creation = " << deltaT << " ms" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds (2));
+    clear_console();
 
     //Method to play sound in real time with the custom stream
+#ifdef REALTIME
     C0deTrackerStream cts;
-    cts.init(track);
+    cts.init(tracks[index]);
     cts.play();
-
+    clear_console();
 
     while(cts.getStatus() == sf::SoundSource::Status::Playing){
-        std::this_thread::sleep_for(std::chrono::seconds (1000000));//sleep to reduce CPU usage
+        int select = index;
+        std::cout<<""<<std::endl;std::cout<<""<<std::endl;std::cout<<""<<std::endl;
+        for(uint8_t i = 0; i<NUMBER_OF_TRACKS; ++i){
+            std::cout << "\t\t";
+            if(i == index){
+                std::cout << " * ";
+            }else{
+                std::cout << " " << +i <<". ";
+            }
+            std::cout << tracks[i]->getName() << std::endl;
+        }
+        std::cout << "Select track number : ";
+        std::cin >> select;
+        if(select != index  && +NUMBER_OF_TRACKS - +select > 0){
+            index = select;
+            std::cout << " Track selected " << tracks[index]->getName() << std::endl;
+            cts.changeTrack(tracks[index]);
+            cts.play();
+            std::this_thread::sleep_for(std::chrono::seconds (1));
+        }
+        std::this_thread::sleep_for(std::chrono::seconds (1));//sleep to reduce CPU usage
+        clear_console();
     }
     /************************************************************/
-
+#else
     //Method to save in a file the song. Comment previous method to save song in file.
+    std::string FILENAME(track->getName());
+    FILENAME += ".wav";
     sf::SoundBuffer buffer;
     std::vector<sf::Int16> samples;
     std::cout << "Begin sampling" << std::endl;
 
-    float duration_in_sec = 4.f*2;
-    float number_of_samples = SAMPLE_RATE * duration_in_sec * PANNING;
+    float number_of_samples = SAMPLE_RATE * TIME_RECORDING_SECONDS * PANNING;
     samples.reserve(number_of_samples);
 
     for (uint_fast64_t i = 0; i < number_of_samples; ++++i) {
@@ -59,7 +102,7 @@ int main() {
 
 
     delete track;
-
+#endif
 
     return 0;
 }
